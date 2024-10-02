a3c59694a4646db7a8dd7a6af053925930b1b6ac
Pontus <pontaflod95@hotmail.com>
2024-10-02 11:16:07 +0200

    Created class Person I created a new class Person to store the name. I kept the List "names" for the sake of the List & String methods available that otherwise doesn't work if the list contains an object.

--- gitUppgift6/Program.cs
+++ gitUppgift6/Program.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
 
 namespace NameSorter
 {
+    class Person
+    {
+        public string Name { get; set; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            List<string> names = new List<string> { "Anna", "John", "Alice" };
+            List<string> names = new List<string>(); // I create a list holding only names for the sake of String & List methods that doesn't work when the List contains objects.
+            List<Person> persons = new List<Person>(); // I create a list containing all our person objects.
+            persons.Add(new Person("Anna")); // Add a new Person object.
+            persons.Add(new Person("John")); // Add a new Person object.
+            persons.Add(new Person("Alice")); // Add a new Person object.
+            foreach (Person person in persons) // For each Person object we have, we add each Person's name to the list.
+            {
+                names.Add(person.Name);
+            }
+
             Console.WriteLine("Original list:");
             foreach (var name in names)
             {
@@ -26,7 +47,8 @@ namespace NameSorter
             if (names.Contains(searchName))
             {
                 Console.WriteLine($"{searchName} is in the list.");
-            } else
+            }
+            else
             {
                 Console.WriteLine($"{searchName} is not in the list.");
             }
